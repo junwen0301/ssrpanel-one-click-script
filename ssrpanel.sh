1c8fe5e27f8fd0caac74b28e732357d0e19ed576
marisn <marisn@67cc.cn>
2017-11-16 18:19:32 +0800

    Uninstall_Aliyun

--- ssrpanel.sh
+++ ssrpanel.sh
@@ -242,6 +242,149 @@ function install_BBR(){
 function install_RS(){
      wget -N --no-check-certificate https://github.com/91yun/serverspeeder/raw/master/serverspeeder.sh && bash serverspeeder.sh
 }
+function Uninstall_Aliyun(){
+var=`lsb_release -a | grep Gentoo`
+if [ -z "${var}" ]; then 
+	var=`cat /etc/issue | grep Gentoo`
+fi
+
+if [ -d "/etc/runlevels/default" -a -n "${var}" ]; then
+	LINUX_RELEASE="GENTOO"
+else
+	LINUX_RELEASE="OTHER"
+fi
+
+stop_aegis(){
+	killall -9 aegis_cli >/dev/null 2>&1
+	killall -9 aegis_update >/dev/null 2>&1
+	killall -9 aegis_cli >/dev/null 2>&1
+	killall -9 AliYunDun >/dev/null 2>&1
+	killall -9 AliHids >/dev/null 2>&1
+	killall -9 AliYunDunUpdate >/dev/null 2>&1
+    printf "%-40s %40s\n" "Stopping aegis" "[  OK  ]"
+}
+
+remove_aegis(){
+if [ -d /usr/local/aegis ];then
+    rm -rf /usr/local/aegis/aegis_client
+    rm -rf /usr/local/aegis/aegis_update
+	rm -rf /usr/local/aegis/alihids
+fi
+}
+
+uninstall_service() {
+   
+   if [ -f "/etc/init.d/aegis" ]; then
+		/etc/init.d/aegis stop  >/dev/null 2>&1
+		rm -f /etc/init.d/aegis 
+   fi
+
+	if [ $LINUX_RELEASE = "GENTOO" ]; then
+		rc-update del aegis default 2>/dev/null
+		if [ -f "/etc/runlevels/default/aegis" ]; then
+			rm -f "/etc/runlevels/default/aegis" >/dev/null 2>&1;
+		fi
+    elif [ -f /etc/init.d/aegis ]; then
+         /etc/init.d/aegis  uninstall
+	    for ((var=2; var<=5; var++)) do
+			if [ -d "/etc/rc${var}.d/" ];then
+				 rm -f "/etc/rc${var}.d/S80aegis"
+		    elif [ -d "/etc/rc.d/rc${var}.d" ];then
+				rm -f "/etc/rc.d/rc${var}.d/S80aegis"
+			fi
+		done
+    fi
+
+}
+
+stop_aegis
+uninstall_service
+remove_aegis
+
+printf "%-40s %40s\n" "Uninstalling aegis"  "[  OK  ]"
+
+var=`lsb_release -a | grep Gentoo`
+if [ -z "${var}" ]; then 
+	var=`cat /etc/issue | grep Gentoo`
+fi
+
+if [ -d "/etc/runlevels/default" -a -n "${var}" ]; then
+	LINUX_RELEASE="GENTOO"
+else
+	LINUX_RELEASE="OTHER"
+fi
+
+stop_aegis(){
+	killall -9 aegis_cli >/dev/null 2>&1
+	killall -9 aegis_update >/dev/null 2>&1
+	killall -9 aegis_cli >/dev/null 2>&1
+    printf "%-40s %40s\n" "Stopping aegis" "[  OK  ]"
+}
+
+stop_quartz(){
+	killall -9 aegis_quartz >/dev/null 2>&1
+        printf "%-40s %40s\n" "Stopping quartz" "[  OK  ]"
+}
+
+remove_aegis(){
+if [ -d /usr/local/aegis ];then
+    rm -rf /usr/local/aegis/aegis_client
+    rm -rf /usr/local/aegis/aegis_update
+fi
+}
+
+remove_quartz(){
+if [ -d /usr/local/aegis ];then
+	rm -rf /usr/local/aegis/aegis_quartz
+fi
+}
+
+
+uninstall_service() {
+   
+   if [ -f "/etc/init.d/aegis" ]; then
+		/etc/init.d/aegis stop  >/dev/null 2>&1
+		rm -f /etc/init.d/aegis 
+   fi
+
+	if [ $LINUX_RELEASE = "GENTOO" ]; then
+		rc-update del aegis default 2>/dev/null
+		if [ -f "/etc/runlevels/default/aegis" ]; then
+			rm -f "/etc/runlevels/default/aegis" >/dev/null 2>&1;
+		fi
+    elif [ -f /etc/init.d/aegis ]; then
+         /etc/init.d/aegis  uninstall
+	    for ((var=2; var<=5; var++)) do
+			if [ -d "/etc/rc${var}.d/" ];then
+				 rm -f "/etc/rc${var}.d/S80aegis"
+		    elif [ -d "/etc/rc.d/rc${var}.d" ];then
+				rm -f "/etc/rc.d/rc${var}.d/S80aegis"
+			fi
+		done
+    fi
+
+}
+stop_aegis
+stop_quartz
+uninstall_service
+remove_aegis
+remove_quartz
+printf "%-40s %40s\n" "Uninstalling aegis_quartz"  "[  OK  ]"
+pkill aliyun-service
+rm -fr /etc/init.d/agentwatch /usr/sbin/aliyun-service
+rm -rf /usr/local/aegis*
+iptables -I INPUT -s 140.205.201.0/28 -j DROP
+iptables -I INPUT -s 140.205.201.16/29 -j DROP
+iptables -I INPUT -s 140.205.201.32/28 -j DROP
+iptables -I INPUT -s 140.205.225.192/29 -j DROP
+iptables -I INPUT -s 140.205.225.200/30 -j DROP
+iptables -I INPUT -s 140.205.225.184/29 -j DROP
+iptables -I INPUT -s 140.205.225.183/32 -j DROP
+iptables -I INPUT -s 140.205.225.206/32 -j DROP
+iptables -I INPUT -s 140.205.225.205/32 -j DROP
+iptables -I INPUT -s 140.205.225.195/32 -j DROP
+iptables -I INPUT -s 140.205.225.204/32 -j DROP
+}
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 ulimit -c 0
@@ -269,6 +412,7 @@ echo "#4.  One click Install Serverspeeder                                      
 echo "#5.  Upgrade to the latest ssr-panel [official update script]               #"
 echo "#6.  Log analysis (currently only single node single node support)          #" 
 echo "#7.  One click change the Database password                                 #" 
+echo "#8.  Uninstall aliyun shield monitor & shield cloud shield IP               #"  
 echo "#                      PS:Please build acceleration and build ssrpanel first#"
 echo "#                                     Apply to Centos 7. X system           #"
 echo "#############################################################################"
@@ -302,4 +446,8 @@ fi;
 if [[ $num == "7" ]]
 then
 change_password
+fi;
+if [[ $num == "8" ]]
+then
+Uninstall_Aliyun
 fi;
